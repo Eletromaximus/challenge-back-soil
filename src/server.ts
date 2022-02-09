55e7341b058d6c8f9ff660f0b659c80c2a5e684f
max <maxmillianox@protonmail.com>
2022-02-09 12:07:20 -0300

    chore(primeiro commit): configuraçoes iniciais
    padronizaçao de commits e lint

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import express from 'express'
+// import router from './routes'
+import cors from 'cors'
+import 'express-async-errors'
+// import handleError from './utils/HandleError'
+import helmet from 'helmet'
+
+const app = express()
+
+app.use(express.json())
+
+app.use(helmet())
+
+app.use(cors())
+
+// app.use(router)
+
+// app.use(handleError)
+
+app.listen(process.env.PORT || 4000, () => {
+  console.log('Bem vindo ao server')
+})
