381d64459db64812ffc0ba399097668accd1c5b3
max <maxmillianox@protonmail.com>
2022-02-13 23:45:11 -0300

    fix(apenas uma linha): connection de uma linha

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,7 @@
 /* eslint-disable node/no-path-concat */
 module.exports = {
   client: 'pg',
-  connection: {
-    database: 'd3mqkbj9lib09m' || 'max',
-    user: 'dplqnvapxjylbr',
-    password: '6d3cc1ceb0ea408da56fb8088643bacac0b0588920d1fa04bcf91c8756e79abf',
-    port: 5432
-  },
+  connection: 'postgres://dplqnvapxjylbr:6d3cc1ceb0ea408da56fb8088643bacac0b0588920d1fa04bcf91c8756e79abf@ec2-35-175-68-90.compute-1.amazonaws.com:5432/d3mqkbj9lib09m',
   migrations: {
     tableName: 'knex_migrations',
     directory: `${__dirname}/src/database/migrations`
