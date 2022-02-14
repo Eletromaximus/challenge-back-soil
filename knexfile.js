659376151ecd36cf73ec61af8ab61bc19767c37a
max <maxmillianox@protonmail.com>
2022-02-13 23:05:42 -0300

    fix(objeto connection): objeto connection

--- knexfile.js
+++ knexfile.js
@@ -1,20 +1,23 @@
 /* eslint-disable node/no-path-concat */
 module.exports = {
-  client: 'pg',
-  connection: {
-    user: process.env.USER_NAME,
-    database: process.env.DATABASE_URL || 'max',
-    password: process.env.USER_PASSWORD || '48273#Xz'
-  },
-  migrations: {
-    tableName: 'knex_migrations',
-    directory: `${__dirname}/src/database/migrations`
-  },
-  seeds: {
-    directory: `${__dirname}/src/database/seeds`
-  },
-  pool: {
-    min: 2,
-    max: 10
+  development: {
+    client: 'pg',
+    connection: {
+      user: process.env.USER_NAME,
+      database: process.env.DATABASE_URL || 'max',
+      password: process.env.USER_PASSWORD || '48273#Xz'
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: `${__dirname}/src/database/migrations`
+    },
+    seeds: {
+      directory: `${__dirname}/src/database/seeds`
+    },
+    pool: {
+      min: 2,
+      max: 10
+    }
   }
+
 }
