90cba6774bd988ef15a88095427530d15939d6f5
max <maxmillianox@protonmail.com>
2022-02-13 23:25:00 -0300

    fix(sem um objeto): remoçao do objeto development

--- knexfile.js
+++ knexfile.js
@@ -1,23 +1,20 @@
 /* eslint-disable node/no-path-concat */
 module.exports = {
-  development: {
-    client: 'pg',
-    connection: {
-      user: 'dplqnvapxjylbr',
-      database: 'd3mqkbj9lib09m',
-      password: '6d3cc1ceb0ea408da56fb8088643bacac0b0588920d1fa04bcf91c8756e79abf'
-    },
-    migrations: {
-      tableName: 'knex_migrations',
-      directory: `${__dirname}/src/database/migrations`
-    },
-    seeds: {
-      directory: `${__dirname}/src/database/seeds`
-    },
-    pool: {
-      min: 2,
-      max: 10
-    }
+  client: 'pg',
+  connection: {
+    user: process.env.USER_NAME,
+    database: process.env.DATABASE_URL || 'max',
+    password: process.env.USER_PASSWORD || '48273#Xz'
+  },
+  migrations: {
+    tableName: 'knex_migrations',
+    directory: `${__dirname}/src/database/migrations`
+  },
+  seeds: {
+    directory: `${__dirname}/src/database/seeds`
+  },
+  pool: {
+    min: 2,
+    max: 10
   }
-
 }
