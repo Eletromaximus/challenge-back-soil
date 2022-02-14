b4d57adb895fdd4497893f9f63a818875241a647
max <maxmillianox@protonmail.com>
2022-02-13 23:18:04 -0300

    fix(texto puro): texto puro

--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,11 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection: {},
+    connection: {
+      user: 'dplqnvapxjylbr',
+      database: 'd3mqkbj9lib09m',
+      password: '6d3cc1ceb0ea408da56fb8088643bacac0b0588920d1fa04bcf91c8756e79abf'
+    },
     migrations: {
       tableName: 'knex_migrations',
       directory: `${__dirname}/src/database/migrations`
