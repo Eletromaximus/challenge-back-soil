fab51d17283231715a0c2d6a8e0ddafaf2ed4df0
Max Milliano <maxmillianox@protonmail.com>
2022-02-17 12:29:19 -0300

    fix(antigo modelo): antigo modelo

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,22 @@
 /* eslint-disable node/no-path-concat */
 module.exports = {
   client: 'pg',
-  connection: process.env.DATABASE_URL,
-  searchPath: ['knex', 'public']
+  connection: {
+    host: '127.0.0.1',
+    database: process.env.DATABASE,
+    user: process.env.USER_NAME,
+    password: process.env.USER_PASSWORD || '48273#Xz',
+    port: 5432
+  },
+  migrations: {
+    tableName: 'knex_migrations',
+    directory: `${__dirname}/src/database/migrations`
+  },
+  seeds: {
+    directory: `${__dirname}/src/database/seeds`
+  },
+  pool: {
+    min: 2,
+    max: 10
+  }
 }
