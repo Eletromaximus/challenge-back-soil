f715482776f53f3d0e1f8d94c94cf6eb9e771c13
Max Milliano <maxmillianox@protonmail.com>
2022-02-17 15:13:28 -0300

    fix(ssl dentro de connections): ssl dentro de connections

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,18 @@
 /* eslint-disable node/no-path-concat */
 module.exports = {
   client: 'pg',
-  connection: process.env.DATABASE_URL,
+  connection: {
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    ssl: { rejectUnauthorized: false}
+  },
   migrations: {
     tableName: 'knex_migrations',
     directory: `${__dirname}/src/database/migrations`
   },
   seeds: {
     directory: `${__dirname}/src/database/seeds`
-  },
-  ssl: true
+  }
 }
