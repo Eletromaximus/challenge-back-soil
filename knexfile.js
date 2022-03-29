23db583f62563eb3be966af28b68ef243ffc8a19
Max Milliano <maxmillianox@protonmail.com>
2022-03-28 21:43:42 -0300

    feat(separando development e production): alteraçao em knexfille

--- knexfile.js
+++ knexfile.js
@@ -1,18 +1,37 @@
 /* eslint-disable node/no-path-concat */
 module.exports = {
-  client: 'pg',
-  connection: {
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-    ssl: { rejectUnauthorized: true}
+  development: {
+    client: 'pg',
+    connection: {
+      host: process.env.HOST,
+      user: process.env.USER,
+      password: process.env.PASSWORD,
+      database: process.env.DATABASE,
+      ssl: { rejectUnauthorized: false }
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: `${__dirname}/src/database/migrations`
+    },
+    seeds: {
+      directory: `${__dirname}/src/database/seeds`
+    }
   },
-  migrations: {
-    tableName: 'knex_migrations',
-    directory: `${__dirname}/src/database/migrations`
-  },
-  seeds: {
-    directory: `${__dirname}/src/database/seeds`
+  production: {
+    client: 'pg',
+    connection: {
+      host: process.env.HOST,
+      user: process.env.USER,
+      password: process.env.PASSWORD,
+      database: process.env.DATABASE,
+      ssl: { rejectUnauthorized: false }
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: `${__dirname}/src/database/migrations`
+    },
+    seeds: {
+      directory: `${__dirname}/src/database/seeds`
+    }
   }
 }
